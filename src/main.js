439ea49f1e8141cdfc122e7eda49ee5f1a91eb13
SadeqSp <sadeq.sp@gmail.com>
2021-05-06 14:00:00 +0430

    7-end

--- src/main.js
+++ src/main.js
@@ -3,6 +3,30 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// automatic global registration
+// https://vuejs.org/v2/guide/components-registration.html#Automatic-Global-Registration-of-Base-Components
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+
+const requireComponent = require.context(
+  './components', 
+  false, 
+  /Base[A-Z]\w+\.(vue|js)$/
+)
+
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName)
+  const componentName = upperFirst(
+    camelCase(
+      fileName
+        .split('/')
+        .pop()
+        .replace(/\.\w+$/, '')
+    )
+  )
+  Vue.component(componentName, componentConfig.default || componentConfig)
+})
+
 Vue.config.productionTip = false
 
 new Vue({
