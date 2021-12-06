f02fccf5150e8f416477cd26a7f529c784d87e64
SadeqSp <sadeq.sp@gmail.com>
2021-12-06 13:08:28 +0330

    9-end

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,16 +1,27 @@
 <template>
     <div>
         <h1>event list</h1>
-        <EventCard />
+        <EventCard v-for="event in events" :key="event.id" :event="event" />
     </div>
 </template>
 
 <script>
     import EventCard from '@/components/EventCard.vue';
-    
+    import EventService from '@/services/EventService.js'
+
     export default {
+        data() {
+            return {
+                events: []
+            }
+        },
         components: {
             EventCard
         },
+        created() {
+            EventService.getEvents()
+                        .then( response => { this.events = response.data })
+                        .catch( error => { console.log(error) })
+        }
     };
 </script>
